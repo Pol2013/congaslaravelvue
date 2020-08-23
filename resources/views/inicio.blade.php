0e86f1c88501a66d5cfcf1d6c3b846b34d826084
Pol Cristobal Paredes <ingenieropol.2013@gmail.com>
2020-08-23 13:46:19 -0500

    agregue componente vue al archivo blade

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>@yield('title')- M.D.CONGAS</title>
+<!-- Custom styles for this template -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+  </head>
+
+<body>
+	  
+	 <div id="app" class="content">
+        <example-component></example-component>
+   </div>
+  
+  <script src="{{ asset('js/app.js') }}"></script>
+     
+</body>
+</html>
